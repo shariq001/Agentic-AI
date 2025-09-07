c2450444eef1aaff9003824e7e4ac6ddcc563ec7
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2025-09-07 21:57:21 +0500

    AGENTIC AI

---
+++ Test Practice/02_uv_package_app/src/uv_package_app/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from Muhammad Shariq! - Init File")
---
+++ Test Practice/02_uv_package_app/src/uv_package_app/main.py
@@ -0,0 +1,4 @@
+print("Top-level hello from Muhammad Shariq!")  # runs immediately when file is executed or imported
+
+def main() -> None:
+    print("Hello from Muhammad Shariq! — running main.py")
