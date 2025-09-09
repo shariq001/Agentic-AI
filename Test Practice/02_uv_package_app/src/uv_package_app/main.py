a85daf963caaf683c426d97b54abde0300531cbc
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2025-09-09 22:03:47 +0500

    Update

--- Test Practice/02_uv_package_app/src/uv_package_app/main.py
+++
@@ -1,4 +0,0 @@
-print("Top-level hello from Muhammad Shariq!")  # runs immediately when file is executed or imported
-
-def main() -> None:
-    print("Hello from Muhammad Shariq! — running main.py")
